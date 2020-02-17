f5824911fa4554da07b6f5fa1c313082312b2677
tomek-jankowiak <tomek.jankowiak01@gmail.com>
2020-02-17 16:14:39 +0100

    agent2

--- problem.c
+++ problem.c
@@ -9,9 +9,11 @@
 #include <sys/sem.h>
 
 #define SMOKER_STARTING_MONEY 30
+#define PRICES_SHM_KEY 10000
+#define PRICES_SEM_KEY 20000
 int agentId, tobaccoSmokerId, paperSmokerId, matchesSmokerId;
-int shmid;
-int *prices;
+int shmid, semid;
+int *prices, iterations = 0;
 
 void agent();
 void tobaccoSmoker();
@@ -48,8 +50,27 @@ void lock(int semid, int semnum, int val)
 
 int main()
 {
-    shmid = shmget(11111, 3 * sizeof(int), IPC_CREAT|0600);
+    shmid = shmget(PRICES_SHM_KEY, 3 * sizeof(int), IPC_CREAT|0600);
+    if(shmid == -1)
+    {
+        perror("Utworzenie obszaru pamięci wspoldzielonej");
+        exit(1);
+    }
     prices = (int*)shmat(shmid, NULL, 0); 
+
+    semid = semget(PRICES_SEM_KEY, 1, IPC_CREAT|0600);
+    if(semid == -1)
+    {
+        perror("Utworzenie tablicy semaforow");
+        exit(1);
+    }
+
+    if(semctl(semid, 0, SETVAL, 3) == -1)
+    {
+        perror("Nadanie wartosci semaforowi");
+        exit(1);
+    }
+
     switch(agentId = fork())
     {
         case -1:
@@ -82,6 +103,11 @@ int main()
         case 0:
             matchesSmoker();
     }
+    while(iterations <= 5);
+    kill(agentId, 9);
+    kill(tobaccoSmokerId, 9);
+    kill(paperSmokerId, 9);
+    kill(matchesSmokerId, 9);
 }
 
 void agent()
@@ -89,13 +115,18 @@ void agent()
     srand(time(0));
     for(;;)
     {
+        lock(semid, 0, 3);
         for(int i = 0; i < 3; i++)
         {
             prices[i] = rand() % (SMOKER_STARTING_MONEY / 3) + 1;
             printf("%d ", prices[i]);
         }
+        unlock(semid, 0, 3);
+        iterations++;
         printf("\n");
         sleep(2);
+        if(iterations >= 5)
+            break;
     }
 }
 
